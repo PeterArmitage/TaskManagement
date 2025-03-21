a2a172ee1799b30683a9ca1037b16fd74f658d56
Peter Armitage <ficochu84@gmail.com>
2025-03-21 17:40:34 -0300

    changed config

--- Frontend/src/server.ts
+++ Frontend/src/server.ts
@@ -34,7 +34,7 @@ app.use(
     maxAge: '1y',
     index: false,
     redirect: false,
-  }),
+  })
 );
 
 /**
@@ -44,9 +44,11 @@ app.use('/**', (req, res, next) => {
   angularApp
     .handle(req)
     .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
+      response ? writeResponseToNodeResponse(response, res) : next()
     )
-    .catch(next);
+    .catch(() => {
+      res.status(404).redirect('/404');
+    });
 });
 
 /**
